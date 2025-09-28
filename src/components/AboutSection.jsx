db4496e8346bbb72a1a607aa011776a9f03b7ead
sofiane belhia <sofianebelhia150@gmail.com>
2025-09-28 15:10:26 +0100

    Changed X and insta redirect

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,11 +8,20 @@ import locationSvg from '../assets/svgs/location.svg'
 import profilePic from '../assets/images/image.png'
 
 
+/**
+ * About Section component
+ * 
+ * This component displays information about the author and 
+ * provides links to their social media profiles.
+ * 
+ * @returns {JSX.Element} The About Section component
+ */
 const AboutSection = () => {
     return (
         <section id="about" className="py-20">
             <div className="container mx-auto px-6">
                 <div className="flex flex-col md:flex-row items-center gap-12">
+                    {/* Profile Picture Section */}
                     <div className="md:w-1/3">
                         <div className="w-64 h-64 mx-auto rounded-2xl shadow-2xl">
                             <img
@@ -21,10 +30,16 @@ const AboutSection = () => {
                                 alt="sofiane's ghibli ai generated picture" />
                         </div>
                     </div>
+
+                    {/* About Section */}
                     <div className="md:w-2/3">
                         <h2 className="text-4xl md:text-6xl font-bold mb-6 font-basker">Meet sofiane</h2>
                         <p className="text-lg uppercase tracking-wider mb-6 font-semibold">Frontend Engineer • Web Developer</p>
-                        <p className="text-gray-600 text-lg mb-6">Front-end developer with hands-on experience building responsive web applications using modern technologies including React, JavaScript, and CSS frameworks. Proven ability to create user-friendly interfaces with strong attention to detail. Passionate about continuous learning and staying current with industry best practices. Experience with version control, API integration, and responsive design principles. </p>
+                        <p className="text-gray-600 text-lg mb-6">
+                            Front-end developer with hands-on experience building responsive web applications using modern technologies including React, JavaScript, and CSS frameworks. Proven ability to create user-friendly interfaces with strong attention to detail. Passionate about continuous learning and staying current with industry best practices. Experience with version control, API integration, and responsive design principles.
+                        </p>
+
+                        {/* Location Section */}
                         <p className="text-sm uppercase tracking-wider mb-6 flex items-center gap-1">
                             <a
                                 href="https://www.google.com/maps/place/Sa%C3%AFda/@34.8478796,0.0620833,12z/data=!3m1!4b1!4m6!3m5!1s0x1280178861cd9055:0xa79d79331eb57fc4!8m2!3d34.8381764!4d0.1481254!16zL20vMDVsNnYz?entry=ttu&g_ep=EgoyMDI1MDgyNS4wIKXMDSoASAFQAw%3D%3D"
@@ -34,8 +49,10 @@ const AboutSection = () => {
                             </a>
                             Saida, Algeria
                         </p>
+
+                        {/* Social Media Links Section */}
                         <div className="flex gap-4">
-                            <a href="https://linkedin.com/in/sofiane-belhia" target='_blank' 
+                            <a href="https://linkedin.com/in/sofianebelhia" target='_blank' 
                             className="w-10 h-10 text-white rounded-full flex items-center justify-center hover:scale-135 transition-transform duration-200 ease-in-out">
                                 <img src={linkedinSvg} alt="linkedin icon links to sofiane's linkedin" />
                             </a>
@@ -45,15 +62,15 @@ const AboutSection = () => {
                                 <img src={githubSvg} alt="instagram icon links to sofiane's github" />
                             </a>
                             <a 
-                            href="https://https://www.instagram.com/14.8hz" 
-                            target='_blank' 
-                            className="w-10 h-10 text-white rounded-full flex items-center justify-center hover:scale-135 transition-transform duration-200 ease-in-out">
+                                href="https://www.instagram.com/18.7hz/" 
+                                target='_blank' 
+                                className="w-10 h-10 text-white rounded-full flex items-center justify-center hover:scale-135 transition-transform duration-200 ease-in-out">
                                 <img src={instagramSvg} alt="instagram icon links to sofiane's instagram" />
                             </a>
                             <a 
-                            href="https://x.com/sofianemekki7" 
-                            target='_blank' 
-                            className="w-10 h-10 text-white rounded-full flex items-center justify-center hover:scale-135 transition-transform duration-200 ease-in-out">
+                                href="https://x.com/sofiane_belhia" 
+                                target='_blank' 
+                                className="w-10 h-10 text-white rounded-full flex items-center justify-center hover:scale-135 transition-transform duration-200 ease-in-out">
                                 <img src={xSvg} alt="X icon links to sofiane's X" />
                             </a>
                         </div>
